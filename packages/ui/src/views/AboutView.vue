9047c6440046e9009fdf51a7306e4c33cc1ead74
interestingLSY <interestingLSY@gmail.com>
2023-10-24 10:19:51 +0800

    feat: Change the frontend UI framework to Vuetify

--- packages/ui/src/views/AboutView.vue
+++ packages/ui/src/views/AboutView.vue
@@ -1,4 +1,17 @@
 <template>
+  <v-card
+    class="mx-auto"
+    width="400"
+    prepend-icon="mdi-home"
+  >
+    <template v-slot:title>
+      This is a title
+    </template>
+
+    <v-card-text>
+      This is content
+    </v-card-text>
+  </v-card>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
