e79e0b2ff430763c01b337e3673310618115abb5
Tianle Xu <xtl@xtlsoft.top>
2023-11-08 23:42:23 +0800

    fix(judger): fix several bugs

--- packages/judger/scripting/starlark/init.go
+++ packages/judger/scripting/starlark/init.go
@@ -1,6 +1,8 @@
 package starlark
 
-import "github.com/lcpu-club/paralab/packages/judger/scripting"
+import (
+	"github.com/lcpu-club/paralab/packages/judger/scripting"
+)
 
 func init() {
 	scripting.RegisterEngine(NewStarlarkEngine)
