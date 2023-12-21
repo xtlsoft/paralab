bb38e6e9687295f3b8a1c58a41929aceb5f1a04f
interestingLSY <interestingLSY@gmail.com>
2023-12-21 14:00:11 +0800

    hotfix(ui): fix the bug that rvalue_of_score() throws an error when score > 100

--- packages/ui/src/components/ScoreColor.ts
+++ packages/ui/src/components/ScoreColor.ts
@@ -1,11 +1,11 @@
-export function rvalue_of_score (x : number) { 
-    if (x < 0 || x > 100)
-        throw new Error('x must be in [0, 100]')
+export function rvalue_of_score (x: number) { 
+    if (x < 0) x = 0;
+    if (x > 100) x = 100;
     return 240 - x * 2
 }
 
-export function gvalue_of_score (x : number) {
-    if (x < 0 || x > 100)
-        throw new Error('x must be in [0, 100]')
+export function gvalue_of_score (x: number) {
+    if (x < 0) x = 0;
+    if (x > 100) x = 100;
     return 40 + x * 2
 }
